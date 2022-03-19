c950e08e42479d045d36c1e2154deba2e89c13a7
Poncho1424 <94089758+Poncho1424@users.noreply.github.com>
2022-03-18 23:37:09 -0700

    First commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import "react-native-gesture-handler"
+import {NavigationContainer} from "@react-navigation/native"
+import NavigationStack from "./src/navigation/NavigationStack"
+
+export default function App() {
+  return (
+      <NavigationContainer>
+        <NavigationStack />
+      </NavigationContainer>
+  );
+}
